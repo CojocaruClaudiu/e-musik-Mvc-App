21dc40ab41a917a4dab1fcdb6092ce465c3713e9
claud <claud@KLAU>
2023-01-03 20:54:02 +0200

    Add project files.

---
+++ e-musik-Mvc-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_musik_Mvc_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
